94870ed88664dcf86c51f37fb46e40b1fd812166
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-18 13:53:50 +0530

    feat : change in code

--- extra/patterns/equilateral.js
+++ extra/patterns/equilateral.js
@@ -1,20 +1,20 @@
-let noOfRows=process.argv[2];
+let noOfRows = process.argv[2];
 myFunction(noOfRows);
 function myFunction()
 {
-   if(noOfRows>=2 && noOfRows<=10)
+   if(noOfRows >= 2 && noOfRows <= 10)
    {
-       let i=0;
-       let space=noOfRows-1;
+       let i = 0;
+       let space = noOfRows-1;
        let k,j,l,m,n;
-       for(;i<noOfRows;i++)
+       for(; i < noOfRows; i++)
        {
-            for(k=i;k<noOfRows;k++)
+            for(k = i;k < noOfRows; k++)
             {
                 process.stdout.write(" ");
             }
             space--;
-            for(j=1;j<=(2*i-1);j++)
+            for(j = 1;j <= (2*i)+1; j++)
             {
                process.stdout.write("*");
             }
