a4a26d1a08ec932b3d05e3ec32197a08a30de91f
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-18 19:50:00 +0530

    feat : style : change code format

--- extra/utils/validations.js
+++ extra/utils/validations.js
@@ -47,10 +47,12 @@ function validateEmail(value) {
 function getResult(validUsers, invalidUsers) {
   return (validUsers.count > 0 ?
     (console.log("Valid Count :" + validUsers.count),
-      console.log(validUsers.users.toString())) : console.log("email id not founds"))
+      console.log(validUsers.users.toString())) :
+      console.log("email id not founds"))
     || (invalidUsers.count > 0 ?
       (console.log("invalid Count :" + invalidUsers.count),
-        console.log(invalidUsers.users.toString())) : console.log("email id not founds"));
+        console.log(invalidUsers.users.toString())) :
+        console.log("email id not founds"));
 }
 validateUsers(users);
 
