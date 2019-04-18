736b967d1a61b66b9050622de8788d1d61782366
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-18 19:24:29 +0530

    feat : change in code

---
+++ extra/utils/permission.js
@@ -0,0 +1,19 @@
+let permission = {
+  getUsers: {
+    all: ['head-trainer'],
+    read: ['trainee', 'trainer'],
+    write: ['trainer'],
+    delete: [],
+  }
+}
+function hasPermission(module, role, permissionType) {
+
+  // for (let result1 in permission[module]) {
+  //   for (let result2 in permission[module][result1]) {
+  //     return permission[module][permissionType][result2] == role && permission[module][permissionType]['all']==role;
+  //   }
+  // }
+}
+console.log(hasPermission('getUsers', 'head-trainer', 'delete'));
+
+
