c60d87d02dcea4d129e126f3e5cb0ad7eea1bc6f
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-18 16:09:23 +0530

    feat : add validation code

--- extra/utils/validations.js
+++ extra/utils/validations.js
@@ -1,25 +1,51 @@
-let users=[{
+let validUser = 0;
+let invalidUser = 0;
+let validEmail = [];
+let invalidEmail = [];
+let users = [{
   traineeEmail: 'trainee1@successive.tech',
-  reviewerEmail: 'reviewer1@successive.tech'
-}]
+  reviewerEmail: 'reviewer1@1successive.tech'
+},
+{
+  traineeEmail: 'trainee1.gh.sh#successive.tech',
+  reviewerEmail: 'reviewer1-s@successive.tech'
+}
+]
 validateUsers(users)
-function validateUsers(users){
-  users.forEach((value)=>{
-    validateEmail(value.traineeEmail);
+function validateUsers(users) {
+  users.forEach((value) => {
+    validateEmail(value.traineeEmail)
+    validateEmail(value.reviewerEmail)
   })
+  getResult();
 }
 
-function validateEmail(value){
-  let expression="/^\w+([/.-]?\w+)*@\w+([/.-]?\w+)*(\.\w[2,4])+$/";
-  if(expression.value.match(value))
-  {
+function validateEmail(value) {
+  let expression = "/^\w+([/.-]?\w+)*@\w+([/.-]?\w+)*(\.\w[2,4])+$/";
+  let domain_name = "@successive.tech$";
+  if (value.match(expression) || value.match(domain_name)) {
+    validUser++;
+    validEmail.push(value);
     return true;
   }
-  else
-  {
+  else {
+    invalidUser++;
+    invalidEmail.push(value);
     return false;
   }
+}
 
+function getResult() {
+  if (validUser > 0) {
+    console.log("Valid Count :" + validUser)
+    validEmail.forEach(value => { console.log(value) })
+    validUser = 0;
+  }
+  if (invalidUser > 0) {
+    console.log("invalid Count :" + invalidUser)
+    invalidEmail.forEach(value => { console.log(value) })
+    invalidUser = 0;
+  }
 }
 
 
