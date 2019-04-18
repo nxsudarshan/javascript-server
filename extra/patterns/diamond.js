c95cc40e13d253a36d44998b6a0aba0b10de0fdd
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-18 13:53:40 +0530

    feat : change in code

--- extra/patterns/diamond.js
+++ extra/patterns/diamond.js
@@ -1,33 +1,33 @@
-let noOfRows=process.argv[2];
-myFunction(noOfRows);
-function myFunction()
+let noOfRows = process.argv[2];
+diamond(noOfRows);
+function diamond()
 {
-    if(noOfRows>=2 && noOfRows<=10)
+    if(noOfRows >= 2 && noOfRows <= 10)
    {
-       let i=0;
-       let space=noOfRows-1;
+       let i = 0;
+       let space = noOfRows-1;
        let k,j,l,m,n;
-       for(;i<noOfRows;i++)
+       for(;i < noOfRows; i++)
        {
-            for(k=i;k<noOfRows;k++)
+            for(k = i; k < noOfRows; k++)
             {
                 process.stdout.write(" ");
             }
             space--;
-            for(j=1;j<=(2*i-1);j++)
+            for(j = 1;j <= (2*i)+1; j++)
             {
                 process.stdout.write("*")
             }
             console.log(" ")
         }
-        space=1;
-        for(l=1;l<=noOfRows-1;l++)
+        space = 1;
+        for(l = 1;l <= noOfRows; l++)
         {
             for(m=1;m<=space;m++){ 
                 process.stdout.write(" ");
             }
             space++;
-            for(n=1;n<=2*(noOfRows-l)-1;n++)
+            for(n = 1;n <= 2*(noOfRows-l)+1 ;n++)
             {
                 process.stdout.write("*");
             }
