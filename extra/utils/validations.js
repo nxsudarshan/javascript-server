eb2eba100fdaa9d90488a6f0688e792b2219c457
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-18 13:41:31 +0530

    feat : add validateEmail function with code

--- extra/utils/validations.js
+++ extra/utils/validations.js
@@ -2,11 +2,24 @@ let users=[{
   traineeEmail: 'trainee1@successive.tech',
   reviewerEmail: 'reviewer1@successive.tech'
 }]
+validateUsers(users)
+function validateUsers(users){
+  users.forEach((value)=>{
+    validateEmail(value.traineeEmail);
+  })
+}
 
-function validateUsers(){
+function validateEmail(value){
+  let expression="/^\w+([/.-]?\w+)*@\w+([/.-]?\w+)*(\.\w[2,4])+$/";
+  if(expression.value.match(value))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 
 }
 
-function validateEmail(){
 
-}
