3402d5378f011e951d1f9458e7117bf10a5fac81
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-19 15:07:23 +0530

    feat : style :  change in format

--- extra/utils/permission.js
+++ extra/utils/permission.js
@@ -10,10 +10,11 @@ function hasPermission(module, role, permissionType) {
 
   for (let result1 in permission[module]) {
     for (let result2 in permission[module][result1]) {
-      return permission[module][permissionType][result2] == role || permission[module]['all'] == role;
+      return permission[module][permissionType][result2] == role ||
+      permission[module]['all'] == role;
     }
   }
 }
-console.log(hasPermission('getUsers', 'trainer', 'delete'));
+console.log(hasPermission('getUsers', 'head-trainer', 'all'));
 
 
