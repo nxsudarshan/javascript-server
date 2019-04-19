b56a9aee74121e8f220cff0ccf8441286708a6fb
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-19 19:48:56 +0530

    feat : style : add style for it

--- extra/index.js
+++ extra/index.js
@@ -1,5 +1,7 @@
 import * as print1 from './patterns/index';
 import * as print2 from './utils/index';
+import { users } from './constants.js';
+
 
 const noOfRows = process.argv[2];
 console.log("------------------------------")
@@ -9,6 +11,12 @@ print1.equilateral(noOfRows);
 console.log("------------------------------")
 
 
-console.log("Permission");
-print2.check();
+console.log("Check Permission");
+print2.checkPermission();
 
+console.log("------------------------------")
+
+console.log("Check Email")
+print2.checkEmailValidate(users)
+
+console.log("------------------------------")
