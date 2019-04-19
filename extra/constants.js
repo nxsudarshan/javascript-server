5804c04814874773f11443f731fcc42e6e55690b
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-19 19:09:51 +0530

    feat : add constants

---
+++ extra/constants.js
@@ -0,0 +1,10 @@
+export const PERMISSION_READ = 'read';
+export const PERMISSION_WRITE = 'write';
+export const PERMISSION_DELETE = 'delete';
+export const PERMISSION_ALL = 'all'
+
+export const MODULE_NAME = 'getUsers';
+
+export const ROLE_TRAINEE = 'trainee';
+export const ROLE_TRAINER = 'trainer';
+export const ROLE_HEAD_TRAINER = 'head-trainer';
