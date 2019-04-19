fdd80bad6a7595f5b4a65f373dd1387b887baf8d
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-19 17:33:44 +0530

    feat : change in functions

--- extra/patterns/index.js
+++ extra/patterns/index.js
@@ -1,11 +1,9 @@
 import * as check from './diamond.js';
-import * as equil from './equilateral.js';
+import * as equli from './equilateral.js';
 
 export function diamond(noOfRows) {
-  check.diamond(noOfRows);
+  check.default(noOfRows)
 }
-
-export function equilateral(noOfRows)
-{
-  equil.equilateral(noOfRows);
+export function equilateral(noOfRows){
+ equli.default(noOfRows)
 }
