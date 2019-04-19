1e49da50d83beaa4ff7a3281e4ca48178eda289e
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-19 19:10:17 +0530

    feat : import utils file

--- extra/index.js
+++ extra/index.js
@@ -1,9 +1,14 @@
-import * as check from './patterns/index';
+import * as print1 from './patterns/index';
+import * as print2 from './utils/index';
 
 const noOfRows = process.argv[2];
+console.log("------------------------------")
+print1.diamond(noOfRows);
+console.log(" ")
+print1.equilateral(noOfRows);
+console.log("------------------------------")
 
-check.diamond(noOfRows);
-console.log("")
-check.equilateral(noOfRows);
 
+console.log("Permission");
+print2.check();
 
