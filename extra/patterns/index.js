f85ba6f0ce248337176600fd7e5409fec7cc791d
sudarshan <sudarshan.gonewar@successive.tech>
2019-04-19 17:22:14 +0530

    feat : add new file

---
+++ extra/patterns/index.js
@@ -0,0 +1,11 @@
+import * as check from './diamond.js';
+import * as equil from './equilateral.js';
+
+export function diamond(noOfRows) {
+  check.diamond(noOfRows);
+}
+
+export function equilateral(noOfRows)
+{
+  equil.equilateral(noOfRows);
+}
